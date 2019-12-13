d4e3fc8b12d6c08309dd98311857b463cf6ad1fe
robertkirkpatrick <rk3505@nyu.edu>
2019-12-12 21:05:48 -0500

    completed tutorial for templates, loading statically defined user and post
    Signed-off-by: robertkirkpatrick <rk3505@nyu.edu>

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
+
+
 def index():
-    return "Hello, World!"
+    user = {'username':'Rob'}
+    posts = [
+        {
+            'author': {'username': 'Brad'},
+            'body': 'I love flask!'
+        },
+        {
+            'author': {'username': 'Jerry'},
+            'body': 'Tom!'
+        }
+    ]
+    # render_template invokes jinja2 template engine
+    return render_template('index.html', title='Home', user=user, posts=posts)
